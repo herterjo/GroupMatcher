b9269bb3035ce7374f05286894c4172b3540a32b
herterjo <herterjo@web.de>
2023-10-03 12:45:56 +0200

    Added variance field to input to easily configure min/max persons in groups independent of actual people count

--- GroupMatcher/Configuration/Input.cs
+++ GroupMatcher/Configuration/Input.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,12 @@ public class Input : BaseInput
 
     public OneToManyAssociation[] OneToManyAssociations { get; set; }
     public int MultipleAssociationsPenalty { get; set; }
+    public uint? GroupMembersVariance { get; set; }
+    public uint? GroupLeadersVariance { get; set; }
+    public uint? FemaleGroupMembersVariance { get; set; }
+    public uint? MaleGroupMembersVariance { get; set; }
+    public uint? FemaleGroupLeadersVariance { get; set; }
+    public uint? MaleGroupLeadersVariance { get; set; }
 
     public BaseInput ToBaseInput()
     {
@@ -30,7 +37,93 @@ public class Input : BaseInput
                     People = new string[] { a.FromPerson, p }
                 }));
         baseInput.ManyAssociations = baseInput.ManyAssociations.Concat(newAssociations).ToArray();
+
+        if (GroupMembersVariance.HasValue && !baseInput.MinGroupMembers.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)(baseInput.FemaleMembers.Length + baseInput.MaleMembers.Length);
+            baseInput.MinGroupMembers = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.GroupMembersVariance.Value, false);
+        }
+        if (GroupMembersVariance.HasValue && !baseInput.MaxGroupMembers.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)(baseInput.FemaleMembers.Length + baseInput.MaleMembers.Length);
+            baseInput.MaxGroupMembers = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.GroupMembersVariance.Value, true);
+        }
+
+        if (GroupLeadersVariance.HasValue && !baseInput.MinGroupLeaders.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)(baseInput.FemaleLeaders.Length + baseInput.MaleLeaders.Length);
+            baseInput.MinGroupLeaders = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.GroupLeadersVariance.Value, false);
+        }
+        if (GroupLeadersVariance.HasValue && !baseInput.MaxGroupLeaders.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)(baseInput.FemaleLeaders.Length + baseInput.MaleLeaders.Length);
+            baseInput.MaxGroupLeaders = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.GroupLeadersVariance.Value, true);
+        }
+
+        if (FemaleGroupMembersVariance.HasValue && !baseInput.MinFemaleGroupMembers.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)baseInput.FemaleMembers.Length;
+            baseInput.MinFemaleGroupMembers = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.FemaleGroupMembersVariance.Value, false);
+        }
+        if (FemaleGroupMembersVariance.HasValue && !baseInput.MaxFemaleGroupMembers.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)baseInput.FemaleMembers.Length;
+            baseInput.MaxFemaleGroupMembers = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.FemaleGroupMembersVariance.Value, true);
+        }
+
+        if (MaleGroupMembersVariance.HasValue && !baseInput.MinMaleGroupMembers.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)baseInput.MaleMembers.Length;
+            baseInput.MinMaleGroupMembers = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.MaleGroupMembersVariance.Value, false);
+        }
+        if (MaleGroupMembersVariance.HasValue && !baseInput.MaxMaleGroupMembers.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)baseInput.MaleMembers.Length;
+            baseInput.MaxMaleGroupMembers = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.MaleGroupMembersVariance.Value, true);
+        }
+
+        if (FemaleGroupLeadersVariance.HasValue && !baseInput.MinFemaleGroupLeaders.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)baseInput.FemaleLeaders.Length;
+            baseInput.MinFemaleGroupLeaders = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.FemaleGroupLeadersVariance.Value, false);
+        }
+        if (FemaleGroupLeadersVariance.HasValue && !baseInput.MaxFemaleGroupLeaders.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)baseInput.FemaleLeaders.Length;
+            baseInput.MaxFemaleGroupLeaders = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.FemaleGroupLeadersVariance.Value, true);
+        }
+
+        if (MaleGroupLeadersVariance.HasValue && !baseInput.MinMaleGroupLeaders.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)baseInput.MaleLeaders.Length;
+            baseInput.MinMaleGroupLeaders = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.MaleGroupLeadersVariance.Value, false);
+        }
+        if (MaleGroupLeadersVariance.HasValue && !baseInput.MaxMaleGroupLeaders.HasValue)
+        {
+            var allRelevantPeopleCount = (uint)baseInput.MaleLeaders.Length;
+            baseInput.MaxMaleGroupLeaders = GetGroupCountLimit(allRelevantPeopleCount, baseInput.GroupCount, this.MaleGroupLeadersVariance.Value, true);
+        }
+
         return baseInput;
+    }
+
+    private static uint? GetGroupCountLimit(uint allRelevantPeopleCount, uint groupCount, uint variance, bool isUpperBound)
+    {
+        var perGroupCount = allRelevantPeopleCount / groupCount;
+        int? limit = (int)perGroupCount;
+        if (isUpperBound)
+        {
+            limit += (int)variance;
+        }
+        else
+        {
+            limit -= (int)variance;
+            if (limit < 0)
+            {
+                limit = null;
+            }
+        }
+        return (uint?) limit;
     }
 
     public static Input ReadConfig(string filePath)
